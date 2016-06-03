c69b7c9d5d5f12b39925abba93536e1c9863ae45
hehaosen <307052084@qq.com>
2016-06-03 10:33:42 +0800

    Initial commit

---
+++ scroll-gpu.js
@@ -0,0 +1,10 @@
+/*
+ * 滚动GPU加速 v0.0.1
+ * Date: 2016-6-3
+ * author:大力神
+ * http://www.github.com/hehaosen
+ */
+var scrollGpu = function (element) {
+    var _element;
+    _element = element ? document.querySelector(element) : document.querySelector('body');
+};
