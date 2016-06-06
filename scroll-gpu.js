59c6b16acde60066e40b20589b3203f6feaa35fa
hehaosen <307052084@qq.com>
2016-06-06 16:26:49 +0800

    弹性滚动优化

--- scroll-gpu.js
+++ scroll-gpu.js
@@ -33,44 +33,58 @@ var scrollGpu = function (element, callback) {
 
     _scrollElement.addEventListener('touchstart',function ( e ) {
 
-        e.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
-            _my = e.touches[0].clientY;
-            _timer = new Date().getTime();
+        e.preventDefault(); // 阻止触摸时浏览器的缩放、滚动条滚动等
+        _my = e.touches[0].clientY;
+        _timer = new Date().getTime();
+        //_scrollElement.style.cssText = 'transform: translate3d(0, ' +  + ', 0)'
     });
     _scrollElement.addEventListener('touchmove',function ( e ) {
 
         if (e.touches[0].clientY - _my + _y > 0 || e.touches[0].clientY - _my + _y < _element.offsetHeight - _scrollElement.offsetHeight) {
-            //如果超出滚动范围，不进行动画
+            // 如果超出滚动范围，不进行动画
             _scrollElement.style.cssText = 'transform: translate3d(0, ' + (e.touches[0].clientY - _my + _y) + 'px, 0);';
         } else {
             _scrollElement.style.cssText = 'transform: translate3d(0, ' + (e.touches[0].clientY - _my + _y) + 'px, 0);';
         }
     });
+
+    var _yAdd = 0, _speedTimer= '';
+
     _scrollElement.addEventListener('touchend',function ( e ) {
         _y = e.changedTouches[0].clientY - _my + _y;
         _timer = new Date().getTime() - _timer;
-        if ( _timer < 500 ) { //500毫秒内滑动有动画
-            if ( _my <= e.changedTouches[0].clientY ) {
-                //下滑操作
-                _y = _y + ( 500 / _timer * (e.changedTouches[0].clientY -_my) );
-                _y = _y > 0 ? 0 : _y;
-                _scrollElement.style.cssText = 'transform: translate3d(0, ' + _y + 'px, 0);transition: -webkit-transform ' + 1 + 's cubic-bezier(0.333333, 0.666667, 0.666667, 1) 0s;';
-            } else {
-                //上滑操作
-                _y = _y - ( 500 / _timer * (_my - e.changedTouches[0].clientY) );
-                _y = _y < _element.offsetHeight - _scrollElement.offsetHeight ? _element.offsetHeight - _scrollElement.offsetHeight : _y;
-                _scrollElement.style.cssText = 'transform: translate3d(0, ' + _y + 'px, 0);transition: -webkit-transform ' + 1 + 's cubic-bezier(0.333333, 0.666667, 0.666667, 1) 0s;';
-            }
+        if ( _y > 0) {
+            _y = 0;
+            _scrollElement.style.cssText = 'transform: translate3d(0, 0, 0)'
+        } else if ( _y < _element.offsetHeight - _scrollElement.offsetHeight) {
+            _y = _element.offsetHeight - _scrollElement.offsetHeight;
+            _scrollElement.style.cssText = 'transform: translate3d(0, ' +  _y + 'px, 0)'
         } else {
-            if ( _y > 0) {
-                _y = 0;
-                _scrollElement.style.cssText = 'transform: translate3d(0, 0, 0)'
-            } else if ( _y < _element.offsetHeight - _scrollElement.offsetHeight) {
-                _y = _element.offsetHeight - _scrollElement.offsetHeight;
-                _scrollElement.style.cssText = 'transform: translate3d(0, ' +  _y + 'px, 0)'
-            }
-        }
+            if ( _timer < 500 ) { // 500毫秒内滑动有动画
+                if ( _my <= e.changedTouches[0].clientY ) {
+                    // 下滑操作
+                    _yAdd = 500 / _timer * (e.changedTouches[0].clientY -_my);
+                    _y = _y + _yAdd;
 
+                    // 如果动画超出范围，动画事件特殊计算
+                    if (_y >= 0) {
+                        _y = 0;
+                        _yAdd = 300 / _timer * _my;
+                    }
+                } else {
+                    // 上滑操作
+                    _yAdd = 500 / _timer * (_my - e.changedTouches[0].clientY);
+                    _y = _y - _yAdd;
+
+                    // 如果动画超出范围，动画事件特殊计算
+                    if (_y < _element.offsetHeight - _scrollElement.offsetHeight) {
+                        _y = _element.offsetHeight - _scrollElement.offsetHeight;
+                        _yAdd = 300 / _timer * (_element.offsetHeight  - _my);
+                    }
+                }
+            }
+            _scrollElement.style.cssText = 'transform: translate3d(0, ' +  _y + 'px, 0);' + 'transition: -webkit-transform ' + ((_yAdd / 1500) < 0.45 ? 0.45 : (_yAdd / 1500)) + 's cubic-bezier(0.333333, 0.666667, 0.666667, 1) 0s;';
+        }
 
     });
 
