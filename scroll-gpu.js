19d1fe963969e8272b71b3843e9e32181706e08d
hehaosen <307052084@qq.com>
2016-06-06 15:21:34 +0800

    +简单的弹性滚动

--- scroll-gpu.js
+++ scroll-gpu.js
@@ -28,24 +28,50 @@ var scrollGpu = function (element, callback) {
     // 保存现有的style内联样式
     var _y = 0, _my;
 
+    // 点击事件监控
+    var _timer;
+
     _scrollElement.addEventListener('touchstart',function ( e ) {
 
         e.preventDefault(); //阻止触摸时浏览器的缩放、滚动条滚动等
             _my = e.touches[0].clientY;
+            _timer = new Date().getTime();
     });
     _scrollElement.addEventListener('touchmove',function ( e ) {
-        _scrollElement.style.cssText = 'transform: translate3d(0, ' + (e.touches[0].clientY - _my + _y) + 'px, 0)'
-    });
 
+        if (e.touches[0].clientY - _my + _y > 0 || e.touches[0].clientY - _my + _y < _element.offsetHeight - _scrollElement.offsetHeight) {
+            //如果超出滚动范围，不进行动画
+            _scrollElement.style.cssText = 'transform: translate3d(0, ' + (e.touches[0].clientY - _my + _y) + 'px, 0);';
+        } else {
+            _scrollElement.style.cssText = 'transform: translate3d(0, ' + (e.touches[0].clientY - _my + _y) + 'px, 0);';
+        }
+    });
     _scrollElement.addEventListener('touchend',function ( e ) {
         _y = e.changedTouches[0].clientY - _my + _y;
-        if ( _y > 0) {
-            _y = 0;
-            _scrollElement.style.cssText = 'transform: translate3d(0, 0, 0)'
-        } else if ( _y < _element.offsetHeight - _scrollElement.offsetHeight) {
-            _y = _element.offsetHeight - _scrollElement.offsetHeight;
-            _scrollElement.style.cssText = 'transform: translate3d(0, ' +  _y + 'px, 0)'
+        _timer = new Date().getTime() - _timer;
+        if ( _timer < 500 ) { //500毫秒内滑动有动画
+            if ( _my <= e.changedTouches[0].clientY ) {
+                //下滑操作
+                _y = _y + ( 500 / _timer * (e.changedTouches[0].clientY -_my) );
+                _y = _y > 0 ? 0 : _y;
+                _scrollElement.style.cssText = 'transform: translate3d(0, ' + _y + 'px, 0);transition: -webkit-transform ' + 1 + 's cubic-bezier(0.333333, 0.666667, 0.666667, 1) 0s;';
+            } else {
+                //上滑操作
+                _y = _y - ( 500 / _timer * (_my - e.changedTouches[0].clientY) );
+                _y = _y < _element.offsetHeight - _scrollElement.offsetHeight ? _element.offsetHeight - _scrollElement.offsetHeight : _y;
+                _scrollElement.style.cssText = 'transform: translate3d(0, ' + _y + 'px, 0);transition: -webkit-transform ' + 1 + 's cubic-bezier(0.333333, 0.666667, 0.666667, 1) 0s;';
+            }
+        } else {
+            if ( _y > 0) {
+                _y = 0;
+                _scrollElement.style.cssText = 'transform: translate3d(0, 0, 0)'
+            } else if ( _y < _element.offsetHeight - _scrollElement.offsetHeight) {
+                _y = _element.offsetHeight - _scrollElement.offsetHeight;
+                _scrollElement.style.cssText = 'transform: translate3d(0, ' +  _y + 'px, 0)'
+            }
         }
+
+
     });
 
     // 回调函数
